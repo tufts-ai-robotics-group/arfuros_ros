a4a8085fb3c5d60f6ad7cd5be1e3f0849b2f3dca
faizan-m <faizan.muhammad@tufts.edu>
2018-11-06 21:44:25 -0500

    Prompt Server Implemented

--- src/prompt_server.cpp
+++ src/prompt_server.cpp
@@ -1,20 +1,70 @@
-#include <arfuros/PromptActionAction.h>
+#include "ros/ros.h"
 #include <actionlib/server/simple_action_server.h>
+
+#include <arfuros/PromptActionAction.h>
+#include <arfuros/Prompt.h>
+#include <arfuros/PromptFeedback.h>
+
+#include <sstream>
+#include <string>
 
 typedef actionlib::SimpleActionServer<arfuros::PromptActionAction> Server;
 
-void execute(const arfuros::PromptActionGoalConstPtr& goal, Server* as)  // Note: "Action" is not appended to DoDishes here
+const std::string PROMPT_OUT = "/ARFUROS/Prompt/PromptRequest";
+const std::string PROMPT_IN = "/ARFUROS/Prompt/PromptFeedback";
+
+ros::Publisher promptPub; 
+
+Server *promptServer;
+bool requestUnderway = false;
+
+void execute(const arfuros::PromptActionGoalConstPtr& goal, Server* as)
 {
-  // Do lots of awesome groundbreaking robot stuff here
-  as->setSucceeded();
+    // Create the Request
+    arfuros::Prompt request;
+    request.type = goal->prompt_type;
+    request.text = goal->prompt_text;
+    request.options = goal->options;
+    request.display_duration = goal->max_wait_time;
+
+    //Send the Request
+    promptPub.publish(request);
+
+    //Regulate Feedback/Success/Failure
+    promptServer = as;
+    requestUnderway = true;
+}
+
+void feedbackCallback(const arfuros::PromptFeedback::ConstPtr& inMsg){
+    if(!requestUnderway){
+        return;
+    }
+
+    if(inMsg->waiting){
+        arfuros::PromptActionFeedback fb;
+        fb.device_waiting = true;
+        promptServer->publishFeedback(fb);
+    }
+    else{
+        arfuros::PromptActionResult rp;
+        rp.timeout = inMsg->timeout;
+        rp.selected_option = inMsg->selected_option;
+        promptServer->setSucceeded(rp);
+        requestUnderway = false;
+    }
+
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "prompt_server");
-  ros::NodeHandle n;
-  Server server(n, "prompt", boost::bind(&execute, _1, &server), false);
-  server.start();
-  ros::spin();
-  return 0;
+    ros::init(argc, argv, "prompt_server");
+    ros::NodeHandle n;
+    Server server(n, "prompt", boost::bind(&execute, _1, &server), false);
+    server.start();
+
+    promptPub = n.advertise<arfuros::Prompt>(PROMPT_OUT, 5);
+    ros::Subscriber feedbackSub  = n.subscribe(PROMPT_IN, 5, feedbackCallback);
+
+    ros::spin();
+    return 0;
 }
